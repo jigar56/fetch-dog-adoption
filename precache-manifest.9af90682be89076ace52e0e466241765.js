a897447cd0cc4902fe2e1387cdf1598d96229d47
CStopper56 <jigar.patel.56@outlook.com>
2025-02-26 14:51:55 -0600

    Updates

---
+++ precache-manifest.9af90682be89076ace52e0e466241765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e48ba9a679b56efb876bb0ba75e2a5",
+    "url": "/fetch-dog-adoption/index.html"
+  },
+  {
+    "revision": "37b2af891e6864d4e08a",
+    "url": "/fetch-dog-adoption/static/css/2.fffe831b.chunk.css"
+  },
+  {
+    "revision": "3705a709187203f87085",
+    "url": "/fetch-dog-adoption/static/css/main.ae3d601a.chunk.css"
+  },
+  {
+    "revision": "37b2af891e6864d4e08a",
+    "url": "/fetch-dog-adoption/static/js/2.97e68e78.chunk.js"
+  },
+  {
+    "revision": "3705a709187203f87085",
+    "url": "/fetch-dog-adoption/static/js/main.4056875f.chunk.js"
+  },
+  {
+    "revision": "6e3e82bfb6d55b341d7b",
+    "url": "/fetch-dog-adoption/static/js/runtime~main.991018fd.js"
+  },
+  {
+    "revision": "2cf4c65ae2e497536a440b815e073f01",
+    "url": "/fetch-dog-adoption/static/media/dog.2cf4c65a.jpg"
+  }
+]);
