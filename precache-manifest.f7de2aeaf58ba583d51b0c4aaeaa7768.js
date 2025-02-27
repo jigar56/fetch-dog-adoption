d41278ce91bb485e4df8f55df815dad771eecdc2
CStopper56 <jigar.patel.56@outlook.com>
2025-02-26 18:15:04 -0600

    Updates

--- precache-manifest.f7de2aeaf58ba583d51b0c4aaeaa7768.js
+++ precache-manifest.f7de2aeaf58ba583d51b0c4aaeaa7768.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17b26ca6f038c2c36a63f6d30fdae57f",
+    "revision": "8640741208807f3f7f89f796801d70f4",
     "url": "/fetch-dog-adoption/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fetch-dog-adoption/static/css/2.fffe831b.chunk.css"
   },
   {
-    "revision": "47d8b29de54de539401c",
-    "url": "/fetch-dog-adoption/static/css/main.120dea79.chunk.css"
+    "revision": "1d703054fc487f120bfe",
+    "url": "/fetch-dog-adoption/static/css/main.b8fd9e6d.chunk.css"
   },
   {
     "revision": "a421582a37c81aa00782",
     "url": "/fetch-dog-adoption/static/js/2.40b20c03.chunk.js"
   },
   {
-    "revision": "47d8b29de54de539401c",
-    "url": "/fetch-dog-adoption/static/js/main.66b46500.chunk.js"
+    "revision": "1d703054fc487f120bfe",
+    "url": "/fetch-dog-adoption/static/js/main.66b02801.chunk.js"
   },
   {
     "revision": "6e3e82bfb6d55b341d7b",
